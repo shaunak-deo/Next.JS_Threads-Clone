ab4d6b2903f29b020f17e09fa838d82e7b676028
Shaunak <shaunakdeo66@gmail.com>
2024-02-19 19:33:54 -0500

    Implement UI sidebars for better navigation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
 
 import "../globals.css";
 import LeftSidebar from "@/components/shared/LeftSidebar";
@@ -23,11 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-      appearance={{
-        baseTheme: dark,
-      }}
-    >
+    <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
           <Topbar />
